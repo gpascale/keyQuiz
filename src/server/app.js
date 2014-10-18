cde2e2674fc0998932c724bb3dfd5908a17b490c
Greg Pascale <gpascale@salesforce.com>
2014-10-17 21:21:10 -0700

    foo

--- src/server/app.js
+++ src/server/app.js
@@ -10,7 +10,7 @@ var Note = require(__dirname + '/Note.js');
 app.get('/', function(req, res) {
     console.log("foo " + __dirname);
     fs.readFile(__dirname + '/public/html/index.html', 'utf8', function(err, text) {
-        console.log(text);
+        console.log('text ' + text);
         res.send(text);
     });
 });
